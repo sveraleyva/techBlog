1c67da098a5e2570ab610c7d68c796e591ceff5e
Samantha Leyva <samanthaleyva@Samanthas-MacBook-Air-2.local>
2023-04-05 16:19:09 -0500

    made User module, Login and Logout views for handlebars, made user-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const postRoutes = require("./post-routes.js");
+const userRoutes = require("./user-routes.js");
 
 router.use("/post", postRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,35 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
+const withAuth = require("../utils/auth");
 
-// // route to get all
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ["password"] },
+      order: [["name", "ASC"]],
+    });
+
+    const users = userData.map((project) => project.get({ plain: true }));
+
+    res.render("homepage", {
+      users,
+      logged_in: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Login route
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
+// // route to get all posts
 router.get("/", async (req, res) => {
   const postData = await Post.findAll().catch((err) => {
     res.json(err);
